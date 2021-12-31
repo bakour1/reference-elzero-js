eb844121a2888e763226e38da82165dbe45f688e
bakour <bakour512@gmail.com>
2021-12-31 22:55:22 +0100

    add p

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 
 
-const numbers = [ 4, 9, 16, 25, 29 ];
-console.log( numbers.find( ( e ) => e > 18 ) );
-//
-// const numbers = [ 4, 9, 16, 25, 29 ];
-// console.log( numbers.findIndex( ( e ) => e > 18 ) );      // 3
-
-
-
-
-
-
 
 
 
