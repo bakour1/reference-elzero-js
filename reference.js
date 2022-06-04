ba9a174573d2f639a3bc392cc97d0e3249f28fed
bakour <bakour512@gmail.com>
2022-06-04 20:49:28 +0100

    d

--- reference.js
+++ reference.js
@@ -7479,6 +7479,207 @@ ${ c } ${ d }` );
 }
 // Regular Expression
 {
+  // Syntax
+  {
+    /*
+
+      /pattern/modifier(s);
+      new RegExp("pattern", "modifier(s)")
+
+      Modifiers => Flags
+      i => case-insensitive
+      g => global
+      m => Multiline
+
+      Search Methods
+      - match(Pattern)
+
+      Match
+      -- Matches A String Against a Regular Expression Pattern
+      -- Returns An Array With The Matches
+      -- Returns null If No Match Is Found.
+    */
+    let myString = "Hello Elzero Web School I Love elzero";
+
+    console.log( myString.match( /Elzero/ ) );                    // ['Elzero', index: 6, input: 'Hello Elzero Web School I Love elzero', groups: undefined]
+    console.log( myString.match( /Elzero/i ) );                   // ['Elzero', index: 6, input: 'Hello Elzero Web School I Love elzero', groups: undefined]
+    console.log( myString.match( /Elzero/ig ) );                  // (2) ['Elzero', 'elzero']
+  }
+  // Ranges
+  {
+    let tld = "Com Net Org Info Code Io";
+
+    // (X|Y) => X Or Y
+    console.log( tld.match( /(org|info|io)/i ) );                // (2) ['Org', 'Org', index: 8, input: 'Com Net Org Info Code Io', groups: undefined]
+    console.log( tld.match( /(org|info|io)/ig ) );               // (3) ['Org', 'Info', 'Io']
+    // -------------------------------------------------------------
+
+    let nums = '123#45$67&8910';
+
+    // [0-9] => 0 To 9
+    console.log( nums.match( /[0-9]/ ) );                        // ['1', index: 0, input: '12345678910', groups: undefined]
+    console.log( nums.match( /[0-9]/g ) );                       // (11) ['1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '0']
+    console.log( nums.match( /[2-6]/g ) );                       // (5) ['2', '3', '4', '5', '6']
+    // -------------------------------------------------------------
+
+    // [^0-9] => Any Character Not 0 To 9
+    console.log( nums.match( /[^0-9]/g ) );                      // (3) ['#', '$', '&']
+    console.log( nums.match( /[^2-6]/g ) );                      // (6) ['1', '7', '8', '9', '1', '0']
+    // -------------------------------------------------------------
+
+    let Practice = "Os1 Os1Os Os2 Os8 Os8Os";
+    console.log( Practice.match( /Os[5-9]Os/g ) );               // ['Os8Os']
+    // -------------------------------------------------------------
+
+    let myString = "AaBbcdefG123!234%^&*";
+
+    // [a-z]
+    // [^a-z]
+    console.log( myString.match( /[a-z]/g ) );                   // (6) ['a', 'b', 'c', 'd', 'e', 'f']
+    console.log( myString.match( /[^a-z]/g ) );                  // (14) ['A', 'B', 'G', '1', '2', '3', '!', '2', '3', '4', '%', '^', '&', '*']
+    // -------------------------------------------------------------
+
+    // [A-Z]
+    console.log( myString.match( /[A-Z]/g ) );                   // (3) ['A', 'B', 'G']
+    // [^A-Z]
+    console.log( myString.match( /[^A-Z]/g ) );                  // (17) ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '!', '2', '3', '4', '%', '^', '&', '*']
+    // -------------------------------------------------------------
+
+    // [ace]
+    console.log( myString.match( /[ace]/g ) );                   // (3) ['a', 'c', 'e']
+    // [^ace]
+    console.log( myString.match( /[^ace]/g ) );                  // (17) ['A', 'B', 'b', 'd', 'f', 'G', '1', '2', '3', '!', '2', '3', '4', '%', '^', '&', '*']
+    // -------------------------------------------------------------
+
+    // [a-zA-Z]
+    console.log( myString.match( /[a-zA-Z]/g ) );                // (9) ['A', 'a', 'B', 'b', 'c', 'd', 'e', 'f', 'G']
+    // [^a-zA-Z]
+    console.log( myString.match( /[^a-zA-Z]/g ) );               // (11) ['1', '2', '3', '!', '2', '3', '4', '%', '^', '&', '*']
+    // [^a-z^A-Z]
+    console.log( myString.match( /[^a-z^A-Z]/g ) );              // (10) ['1', '2', '3', '!', '2', '3', '4', '%', '&', '*']
+  }
+  // Character Classes
+  {
+
+    let email = 'O@@@g...com O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
+
+    // . => matches any character, except newline or other line terminators.
+    console.log( email.match( /./g ) );                                            // (59) ['O', '@', '@', '....
+
+    // \w => matches word characters. [a-z, A-Z, 0-9 And Underscore]
+    console.log( email.match( /\w/g ) );                                           // (35) ['O', 'g', 'c', .....
+
+    // \d => matches digits from 0 to 9.
+    console.log( email.match( /\d/g ) );                                           // (2) ['1', '1']
+
+    // \D => matches non-digit characters.
+    console.log( email.match( /\D/g ) );                                           // (57) ['O', '@', '@'....
+
+    // \s => matches whitespace character.
+    console.log( email.match( /\s/g ) );                                           // (6) [' ', ' ', ' ', ' ', ' ', ' ']
+
+    // \S => matches non whitespace character.
+    console.log( email.match( /\S/g ) );                                           // (53) ['O', '@', '@', ...
+
+    let valid = /\w@\w.(com|net)/g;
+    console.log( email.match( valid ) );                                           // (4) ['O@g.com', 'O@g.net', 'A@Y.com', '1@1.com']
+    // -------------------------------------------------------------
+
+    let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
+
+    console.log( names.match( /spam/ig ) );                                        // (6) ['Spam', 'Spam', 'Spam', 'Spam', 'Spam', 'spam']
+
+    // \b => matches at the beginning or end of a word.
+    console.log( names.match( /\bspam/ig ) );                                      // (2) ['Spam', 'Spam']
+    console.log( names.match( /spam\b/ig ) );                                      // (3) ['Spam', 'Spam', 'Spam']
+    console.log( names.match( /(\bspam|spam\b)/ig ) );                             // (5) ['Spam', 'Spam', 'Spam', 'Spam', 'Spam']
+
+    // \B => matches NOT at the beginning/end of a word.
+    console.log( names.match( /\Bspam/ig ) );                                      // (4) ['Spam', 'Spam', 'Spam', 'spam']
+    console.log( names.match( /spam\B/ig ) );                                      // (3) ['Spam', 'Spam', 'spam']
+    console.log( names.match( /(\Bspam|spam\B)/ig ) );                             // (6) ['Spam', 'Spam', 'Spam', 'Spam', 'Spam', 'spam']
+
+
+  }
+  // Quantifiers
+  {
+    let mails = "o@nn.sa osama@gmail.com elzero@gmail.net osama@mail.ru"; // All Emails
+
+    // n+    => One Or More
+    console.log( mails.match( /\w+@\w+.(com|net)/ig ) );                  // (2) ['osama@gmail.com', 'elzero@gmail.net']
+    console.log( mails.match( /\w+@\w+.\w+/ig ) );                        // (4) ['o@nn.sa', 'osama@gmail.com', 'elzero@gmail.net', 'osama@mail.ru']
+
+    // n*    => zero or more
+    let nums = "0110 10 150 05120 0560 350 00";                           // 0 Numbers Or No 0
+    console.log( nums.match( /0\d+0/ig ) );                               // (3) ['0110', '05120', '0560']
+    console.log( nums.match( /0\d*0/ig ) );                               // (4) ['0110', '05120', '0560', '00']
+
+    // n?    => zero or one
+    let urls = "https://google.com http://www.website.net web.com";       // http + https
+    console.log( urls.match( /https?/ig ) );                              // (2) ['https', 'http']
+    console.log( urls.match( /(https?:\/\/)?(www)?\w+.(com|net)/ig ) );   // (3) ['https://google.com', 'website.net', 'web.com']
+
+    let serials = "S100S S3000S S50000S S950000S";
+
+    // n{x}   => Number of
+    console.log( serials.match( /s[0-9]{3}s/ig ) );                       // ['S100S']
+    console.log( serials.match( /s\d{4,5}s/ig ) );                        // (2) ['S3000S', 'S50000S']
+
+    // n{x,y} => Range
+    console.log( serials.match( /s\d{3}s/ig ) );                         // ['S100S']
+
+    // n{x,}  => At Least x
+    console.log( serials.match( /s\d{3,}s/ig ) );                        // (4) ['S100S', 'S3000S', 'S50000S', 'S950000S']
+
+    let myString = "We Love Programming";
+    let names = "1OsamaZ 2AhmedZ 3Mohammed 4MoustafaZ 5GamalZ";
+
+    // $  => End With Something
+    console.log( /ing$/ig.test( myString ) );                           // true
+    console.log( /lz$/ig.test( names ) );                               // true
+
+    // ^  => Start With Something
+    console.log( /^we/ig.test( myString ) );                            // true
+    console.log( /^1O/ig.test( names ) );                               // true
+    console.log( /^\d/ig.test( names ) );                               // true
+
+    // ?= => Followed By Something
+    console.log( names.match( /\d\w{5}/ig ) );                         // (5) ['1Osama', '2Ahmed', '3Moham', '4Moust', '5Gamal']
+    console.log( names.match( /\d\w{5}(?=z)/ig ) );                    // (3) ['1Osama', '2Ahmed', '5Gamal']
+    console.log( names.match( /\d\w{5}(?!z)/ig ) );                    // (2) ['3Moham', '4Moust']
+  }
+  // Test Method
+  {
+    // pattern.test(input)
+    console.log( /(\bspam|spam\b)/ig.test( 'SAmi' ) );                      // false
+    console.log( /(\bspam|spam\b)/ig.test( 'spami' ) );                     // true
+  }
+  // - replace
+  // - replaceAll
+  {
+    let txt = "We Love Programming And @ Because @ Is Amazing";
+
+    console.log( txt.replace( "@", "sami" ) );                         // We Love Programming And sami Because @ Is Amazing
+    console.log( txt.replaceAll( "@", "sami" ) );                      // We Love Programming And sami Because sami Is Amazing
+
+    console.log( txt.replace( /@/ig, "sami" ) );                       // We Love Programming And sami Because sami Is Amazing
+    console.log( txt.replaceAll( /@/ig, "sami" ) );                    // We Love Programming And sami Because sami Is Amazing
+
+
+  }
+  // Form Validation
+  {
+    document.getElementById( 'register' ).onsubmit = function () {
+      let phoneInput = document.getElementById( 'phone' ).value;
+      let phoneRe = /\(\d{4}\)\s\d{3}-\d{4}/;     // (1234) 567-8910
+      let validationResult = phoneRe.test( phoneInput );
+      if ( !validationResult ) {
+        return false;
+      }
+      return true;
+    };
+
+  }
 
 }
 // Classes
@@ -7838,5 +8039,55 @@ Define a class Person with the following properties:
     // Needed Output
     // [1, 2, 3, 4, 5, 6]
   }
+  //  Cascading Subsets arrays
+  {
+    /*
+      each_cons([1,2,3,4], 2)
+        #=> [[1,2], [2,3], [3,4]]
+
+      each_cons([1,2,3,4], 3)
+        #=> [[1,2,3],[2,3,4]]
+    */
+    {
+      const lst = [ 3, 5, 8, 13 ];
+
+      function eachCons ( array, n ) {
+        let arr = [];
+        for ( let i = 0; i < array.length; i++ ) {
+          arr.push( lst.slice( i, i + n ) );
+        }
+        return arr.filter( e => e.length === n );
+      }
+
+      console.log( eachCons( lst, 1 ) );   // [[3], [5], [8], [13]]
+      console.log( eachCons( lst, 2 ) );   // [[3,5], [5,8], [8,13]]
+      console.log( eachCons( lst, 3 ) );   // [[3,5,8], [5,8,13]]
+    }
+    {
+      function eachCons ( array, n ) {
+        const newArr = [];
+        for ( let a = n, i = 0; a <= array.length; i++, a++ ) {
+          newArr.push( array.slice( i, a ) );
+        }
+        return newArr;
+      }
+    }
+  }
+  // objDobleCate
+  {
+    const counts = {};
+    let objDobleCate = {};
+    let result = "";
+    const sampleArray = [ 54, 32, 5, 11, 33, 35, 32, 17, 3, 3, 22, 4, 1, 6, 11, 33 ];
+
+    sampleArray.forEach( function ( x ) {
+      counts[ x ] = ( counts[ x ] || 0 ) + 1;
+      if ( counts[ x ] > 1 ) {
+        objDobleCate[ x ] = counts[ x ];
+        result = Object.keys( objDobleCate ).length;
+      }
+    } );
+  }
+
 
 }
