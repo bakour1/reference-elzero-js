b567f00fbd4e03e0422a3ad218d95c1634b5c8d6
bakour <bakour512@gmail.com>
2022-12-04 00:22:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function periodIsLate ( last, today, cycleLength ) {
-  return new Date( 2022, 8, 20 ) - new Date( 2022, 8, 4 );
+function findChildren ( str ) {
+  const obj = {};
+  let counter = 1, result = '', array = [ ...str.toLowerCase() ].sort();
+
+  for ( const ele of array ) {
+    obj[ ele ] = counter + ( obj[ ele ] || 0 );
+  }
+
+  for ( const key in obj ) {
+    result += key.toUpperCase();
+    result += key.repeat( obj[ key ] - 1 );
+  }
+
+  return result;
 }
 
-console.log( periodIsLate() );
+let str = 'bhsSseeHehhEBsssb';
+console.log( findChildren( str ) );
+
+// BbbHhhhSsssssEeee
